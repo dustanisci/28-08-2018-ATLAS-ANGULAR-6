cf9aa58499814e7307c8bf81afe64f19cde1c244
Eduardo Stanisci <dustanisci@outlook.com>
2018-08-28 18:46:51 -0300

    Update lucro.service.ts

--- src/app/lucro.service.ts
+++ src/app/lucro.service.ts
@@ -5,7 +5,7 @@ import { Http } from '@angular/http';
 export class LucroService {
   constructor(private http: Http) { }
 
-  //Para consumir o webservice, somente por autorização de IP com o banco de dados.
+  
   lista(): Promise<any> {
     return this.http.get('https://www.mocky.io/v2/5b2c010d300000100023487a')
       .toPromise()
